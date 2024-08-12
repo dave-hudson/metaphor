899adc514a9446f7831f0bc27db94322eed85fdc
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-08-12 11:44:56 +0400

    Cleanups
    * Move token definitions into a separate file.
    
    * Don't store build files!

--- src/Lexer.hpp
+++ src/Lexer.hpp
@@ -3,45 +3,9 @@
 
 #include <string>
 #include <map>
-#include <ostream>
+#include <vector>
 
-enum class TokenType {
-    FEATURE,
-    BACKGROUND,
-    SCENARIO,
-    GIVEN,
-    WHEN,
-    THEN,
-    AND,
-    BUT,
-    AS,
-    I,
-    SO,
-    INCLUDE,
-    COMMENT,
-    NEWLINE,
-    WHITESPACE,
-    TEXT,
-    END_OF_FILE
-};
-
-class Token {
-public:
-    Token(TokenType type, std::string value, int line, int column)
-        : type(type), value(std::move(value)), line(line), column(column) {}
-
-    friend std::ostream& operator<<(std::ostream& os, const Token& token) {
-        os << "Token(type=" << static_cast<int>(token.type)
-            << ", value=\"" << token.value << "\", line=" << token.line
-            << ", column=" << token.column << ")";
-        return os;
-    }
-
-    TokenType type;
-    std::string value;
-    int line;
-    int column;
-};
+#include "Token.hpp"
 
 class Lexer {
 public:
---
+++ src/Token.hpp
@@ -0,0 +1,45 @@
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+
+#include <string>
+#include <ostream>
+
+enum class TokenType {
+    FEATURE,
+    BACKGROUND,
+    SCENARIO,
+    GIVEN,
+    WHEN,
+    THEN,
+    AND,
+    BUT,
+    AS,
+    I,
+    SO,
+    INCLUDE,
+    COMMENT,
+    NEWLINE,
+    WHITESPACE,
+    TEXT,
+    END_OF_FILE
+};
+
+class Token {
+public:
+    Token(TokenType type, std::string value, int line, int column)
+        : type(type), value(std::move(value)), line(line), column(column) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Token& token) {
+        os << "Token(type=" << static_cast<int>(token.type)
+            << ", value=\"" << token.value << "\", line=" << token.line
+            << ", column=" << token.column << ")";
+        return os;
+    }
+
+    TokenType type;
+    std::string value;
+    int line;
+    int column;
+};
+
+#endif
